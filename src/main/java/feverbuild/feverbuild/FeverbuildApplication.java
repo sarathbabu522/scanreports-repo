b4c5c68cf6abf89ca63d336600e31fc94889bf75
root <root@ip-172-31-63-239.ec2.internal>
2023-09-15 07:07:35 +0000

    new chnges code

---
+++ src/main/java/feverbuild/feverbuild/FeverbuildApplication.java
@@ -0,0 +1,13 @@
+package feverbuild.feverbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeverbuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeverbuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/feverbuild/feverbuild/FeverbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package feverbuild.feverbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeverbuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
